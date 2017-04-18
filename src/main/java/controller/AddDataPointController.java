791c763f12e0bd540b57af64b3f13c14b6dc86c5
aignatius3 <aignatius3@gatech.edu>
2017-04-18 01:31:34 -0400

    Created AddDataPoint page for City Scientist

---
+++ src/main/java/controller/AddDataPointController.java
@@ -0,0 +1,29 @@
+package main.java.controller;
+
+/**
+ * Created by Ashwin Ignatius on 4/17/2017.
+ */
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import main.java.fxapp.Main;
+import main.java.model.DatabaseRef;
+import main.java.model.UserType;
+import java.io.File;
+
+public class AddDataPointController extends Controller {
+
+    @FXML
+    private ComboBox locName;
+
+    @FXML
+    private DatePicker dateTime;
+
+    @FXML
+    private ComboBox dataType;
+
+    @FXML
+    private TextField dataValue;
+}
