f40903ef60ee40be9981f5521637d3c0d24e10d0
Arber Muharemi <arber.muharemi@gatech.edu>
2017-04-19 17:23:12 -0400

    removed unused code in Main.java

--- src/main/java/fxapp/Main.java
+++ src/main/java/fxapp/Main.java
@@ -48,67 +48,6 @@ public class Main extends Application {
         return db;
     }
 
-        //if(!conn.isClosed())
-    //    System.out.println("Successfully connected to " +
-            //            "MySQL server using TCP/IP...");
-            //stmt = conn.createStatement();
-
-    //        db.rs = db.stmt.executeQuery("SELECT * FROM POI");
-    //        db.rs.beforeFirst();
-    //        int num = 1;
-    //        while (db.rs.next()) {
-    //            System.out.println("Location " + num + ": " + db.rs.getString("LocationName"));
-    //            num++;
-    //        }
-    //    } catch (Exception e) {
-    //        System.err.println("Exception: " + e.getMessage());
-    //        //} finally {
-    //        //    try {
-    //        //        if(db.conn != null) {
-    //        //            conn.close();
-    //        //        }
-    //        //    } catch(SQLException e) {}
-    //        //}
-    //    }
-    //    Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
-    //    primaryStage.setTitle("Login");
-    //    primaryStage.setScene(new Scene(root));
-    //    primaryStage.show();
-    //    this.window = primaryStage;
-    //    System.out.println(window);
-    //}
-
-    @FXML
-    public void handleLoginPressed() throws Exception {
-        //System.out.println(db);
-        //System.out.println(db.preparedStatement);
-        //System.out.println(db.conn);
-        //db.preparedStatement = db.conn.prepareStatement("SELECT * FROM `User` WHERE Username = ? AND PASSWORD = ?");
-        //db.preparedStatement.setString(1, userField.getText());
-        //db.preparedStatement.setString(2, passField.getText());
-        //System.out.println(db.preparedStatement);
-        //db.rs = db.preparedStatement.executeQuery();
-        //if (!(db.rs.first())) {
-        //    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        //    alert.setTitle("Credentials Error");
-        //    alert.setContentText("No user with these credentials exists. Please reenter your information, or if you have not made an account, click 'Register'.");
-        //    alert.showAndWait();
-        //    return;
-        //}
-        //FXMLLoader loader = new FXMLLoader();
-        //loader.setLocation(Main.class.getResource("src2/sample.fxml"));
-        //BorderPane welcomeLayout = loader.load();
-        //System.out.println(window);
-        //window.setTitle("Welcome Page");
-        //Scene welcomeScene = new Scene(welcomeLayout);
-        //window.setScene(welcomeScene);
-        //window.show();
-    }
-
-    public void handleRegisterPressed() {
-
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
