fd17bb9c742c6f15ea8010712b47cb05b3714bf9
aignatius3 <aignatius3@gatech.edu>
2017-04-22 18:09:54 -0400

    Added fxids to POIDetail and set up Controller

--- src/main/java/controller/POIDetailController.java
+++ src/main/java/controller/POIDetailController.java
@@ -1,7 +1,53 @@
 package main.java.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import main.java.model.Type;
+
+import java.io.File;
+
 /**
  * Created by Ashwin Ignatius on 4/22/2017.
  */
 public class POIDetailController extends Controller {
+
+    @FXML
+    private ComboBox<Type> type;
+
+    @FXML
+    private TextField startData;
+
+    @FXML
+    private TextField endData;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private Spinner startHour;
+
+    @FXML
+    private Spinner startMin;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private Spinner endHour;
+
+    @FXML
+    private Spinner endMin;
+
+    private ObservableList<Type> typeList = FXCollections.observableArrayList(Type.Mold, Type.Air_Quality);
+
+    @FXML
+    private void initialize() {
+        type.setItems(typeList);
+
+    }
+
+    @FXML
+    private void handleBackPressed() {myApp.load(new File("../view/ViewPOIScreen.fxml")); }
 }
