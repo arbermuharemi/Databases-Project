6a1fea14be1b936cb670afe5ae22d167e234f705
Rishabh Mahajani <rmahajani3@gatech.edu>
2017-04-22 15:37:26 -0400

    Added all the fx components to the ViewPOIController class

--- src/main/java/controller/ViewPOIController.java
+++ src/main/java/controller/ViewPOIController.java
@@ -2,8 +2,7 @@ package main.java.controller;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import main.java.fxapp.Main;
 import main.java.model.DatabaseRef;
@@ -16,7 +15,30 @@ import java.sql.SQLException;
 public class ViewPOIController extends Controller{
 
     @FXML
+    private ComboBox<String> locName;
+
+    @FXML
+    private ComboBox<String> cityName;
+
+    @FXML
+    private ComboBox<String> stateName;
+
+    @FXML
+    private ComboBox<String> dataType;
+
+    @FXML
+    private TextField zipcode;
+
+    @FXML
+    private CheckBox flagged;
+
+    @FXML
+    private DatePicker dateTime;
+
+    @FXML
     public void handleBackPressed()  {
         myApp.load(new File("../view/CityOfficialHome.fxml"));
     }
+
+
 }
